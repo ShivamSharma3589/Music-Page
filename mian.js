aea7ea2111881d4ad7860bc1c2c3bff657e6cf0a
ShivamSharma3589 <shivayasharma1149@gmail.com>
2021-07-11 01:19:45 +0530

    Music Page

---
+++ mian.js
@@ -0,0 +1,582 @@
+// list goes up here
+const lists = [
+    {
+        image: "url('images/arijit.jpg')",
+        phone: "url('phone/arijit.jpg')",
+        songName: "Ve Maahi",
+        artistName: "Arijit Singh",
+        song: "music/arijit singh/ve maahi.mp3"
+    },
+    {
+        image: "url('images/jubin.jpg')",
+        phone: "url('phone/jubin.jpg')",
+        songName: "Aankh Uthi Mohabbat Ne",
+        artistName: "Jubin Nautiyal",
+        song: "music/arijit singh/mohabat.mp3"
+    },
+    {
+        image: "url('images/maroon.jpg')",
+        phone: "url('phone/maroon.jpg')",
+        songName: "Stereo Hearts",
+        artistName: "Maroon 5",
+        song: "music/maroon 5/stereo hearts.mp3"
+    },
+    {
+        image: "url('images/maroon.jpg')",
+        phone: "url('phone/maroon.jpg')",
+        songName: "Sugar",
+        artistName: "Maroon 5",
+        song: "music/maroon 5/sugar.mp3"
+    },
+    {
+        image: "url('images/maroon.jpg')",
+        phone: "url('phone/maroon.jpg')",
+        songName: "Beautiful Mistakes",
+        artistName: "Maroon 5",
+        song: "music/maroon 5/beautiful mistakes.mp3"
+    },
+    {
+        image: "url('images/maroon.jpg')",
+        phone: "url('phone/maroon.jpg')",
+        songName: "Animals",
+        artistName: "Maroon 5",
+        song: "music/maroon 5/animals.mp3"
+    },
+    {
+        image: "url('images/maroon.jpg')",
+        phone: "url('phone/maroon.jpg')",
+        songName: "Payphone",
+        artistName: "Maroon 5",
+        song: "music/maroon 5/payphone.mp3"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Levitating",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/levitating.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Don't Start Now",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/break my heart.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Break My Heart",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/levitating.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "IDGAF",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/idgaf.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Cool",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/cool.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "One Kiss",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/one kiss.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "No Lie",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/no lie.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Pretty Please",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/pretty please.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Love Again",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/love again.m4a"
+    },
+    {
+        image: "url('images/dua.jpg')",
+        phone: "url('phone/dua.jpg')",
+        songName: "Hallucinate",
+        artistName: "Dua Lipa",
+        song: "music/dua lipa/hallucinate.m4a"
+    },
+    {
+        image: "url('images/taylor.jpg')",
+        phone: "url('phone/taylor.jpg')",
+        songName: "Blank Space",
+        artistName: "Taylor Swift",
+        song: "music/taylor swift/blank space.mp3"
+    },
+    {
+        image: "url('images/taylor.jpg')",
+        phone: "url('phone/taylor.jpg')",
+        songName: "What You Made Me Do",
+        artistName: "Taylor Swift",
+        song: "music/taylor swift/look what you made me do.mp3"
+    },
+    {
+        image: "url('images/taylor.jpg')",
+        phone: "url('phone/taylor.jpg')",
+        songName: "Love Story",
+        artistName: "Taylor Swift",
+        song: "music/taylor swift/love story.mp3"
+    },
+    {
+        image: "url('images/taylor.jpg')",
+        phone: "url('phone/taylor.jpg')",
+        songName: "Wildest Dream",
+        artistName: "Taylor Swift",
+        song: "music/taylor swift/wildest dream.mp3"
+    },
+    {
+        image: "url('images/taylor.jpg')",
+        phone: "url('phone/taylor.jpg')",
+        songName: "Willow",
+        artistName: "Taylor Swift",
+        song: "music/taylor swift/willow.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "Call You Mine",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/call u mine.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "Break My Heart",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/break my heart.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "Sacrifice",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/sacrifice.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "Hey Mama",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/hey mama.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "I Got You",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/i got you.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "I'm a Mess",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/i m a mess.mp3"
+    },
+    {
+        image: "url('images/bebe.jpg')",
+        phone: "url('phone/bebe.jpg')",
+        songName: "No Broken Hearts",
+        artistName: "Bebe Rexha",
+        song: "music/bebe rexha/no broken hearts.m4a"
+    },
+    {
+        image: "url('images/ariana.jpg')",
+        phone: "url('phone/ariana.jpg')",
+        songName: "Love Me Harder",
+        artistName: "Ariana Grande",
+        song: "music/ariana grande/love me harder.mp3"
+    },
+    {
+        image: "url('images/ariana.jpg')",
+        phone: "url('phone/ariana.jpg')",
+        songName: "7 Rings",
+        artistName: "Ariana Grande",
+        song: "music/ariana grande/7 rings.mp3"
+    },
+    {
+        image: "url('images/ariana.jpg')",
+        phone: "url('phone/ariana.jpg')",
+        songName: "Into You",
+        artistName: "Ariana Grande",
+        song: "music/ariana grande/into you.mp3"
+    },
+    {
+        image: "url('images/ariana.jpg')",
+        phone: "url('phone/ariana.jpg')",
+        songName: "Positions",
+        artistName: "Ariana Grande",
+        song: "music/ariana grande/positions.mp3"
+    },
+    {
+        image: "url('images/ariana.jpg')",
+        phone: "url('phone/ariana.jpg')",
+        songName: "Rain On Me",
+        artistName: "Ariana Grande",
+        song: "music/ariana grande/rain on me.m4a"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Peaches",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/peaches.mp3"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Company",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/company.mp3"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Despacito",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/despacito.mp3"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Intentions",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/intentions.m4a"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Let Me Love You",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/let me love you.mp3"
+    },
+    {
+        image: "url('images/justin.jpg')",
+        phone: "url('phone/justin.jpg')",
+        songName: "Sorry",
+        artistName: "Justin Bieber",
+        song: "music/justin bieber/Sorry.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "On My Way",
+        artistName: "Alan Walker",
+        song: "music/alan walker/on my way.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Alone",
+        artistName: "Alan Walker",
+        song: "music/alan walker/alone.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Alone Part II",
+        artistName: "Alan Walker",
+        song: "music/alan walker/alone2.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Choir",
+        artistName: "Alan Walker",
+        song: "music/alan walker/choir.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Darkside",
+        artistName: "Alan Walker",
+        song: "music/alan walker/darkside.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Faded",
+        artistName: "Alan Walker",
+        song: "music/alan walker/faded.mp3"
+    },
+    {
+        image: "url('images/alan.jpg')",
+        phone: "url('phone/alan.jpg')",
+        songName: "Ignite",
+        artistName: "Alan Walker",
+        song: "music/alan walker/ignite.mp3"
+    },
+    {
+        image: "url('images/chain.jpg')",
+        phone: "url('phone/chain.jpg')",
+        songName: "Closer",
+        artistName: "Chain Smokers",
+        song: "music/chain smoker/closer.mp3"
+    },
+    {
+        image: "url('images/chain.jpg')",
+        phone: "url('phone/chain.jpg')",
+        songName: "Don't Let Me Down",
+        artistName: "Chain Smokers",
+        song: "music/chain smoker/dont let me down.mp3"
+    },
+    {
+        image: "url('images/chain.jpg')",
+        phone: "url('phone/chain.jpg')",
+        songName: "All We Know",
+        artistName: "Chain Smokers",
+        song: "music/chain smoker/all we know.mp3"
+    },
+    {
+        image: "url('images/chain.jpg')",
+        phone: "url('phone/chain.jpg')",
+        songName: "Hope",
+        artistName: "Chain Smokers",
+        song: "music/chain smoker/hope.mp3"
+    },
+    {
+        image: "url('images/chain.jpg')",
+        phone: "url('phone/chain.jpg')",
+        songName: "Something Just Like This",
+        artistName: "Chain Smokers",
+        song: "music/chain smoker/closer.mp3"
+    },
+    {
+        image: "url('images/charlie.jpg')",
+        phone: "url('phone/charlie.jpg')",
+        songName: "We Don't Talk Anymore",
+        artistName: "Charlie Puth",
+        song: "music/charlie puth/we dont talk.mp3"
+    },
+    {
+        image: "url('images/charlie.jpg')",
+        phone: "url('phone/charlie.jpg')",
+        songName: "Attention",
+        artistName: "Charlie Puth",
+        song: "music/charlie puth/attention.mp3"
+    },
+    {
+        image: "url('images/charlie.jpg')",
+        phone: "url('phone/charlie.jpg')",
+        songName: "One Call Away",
+        artistName: "Charlie Puth",
+        song: "music/charlie puth/one call away.mp3"
+    },
+    {
+        image: "url('images/charlie.jpg')",
+        phone: "url('phone/charlie.jpg')",
+        songName: "How Long",
+        artistName: "Charlie Puth",
+        song: "music/charlie puth/how long.mp3"
+    },
+    {
+        image: "url('images/charlie.jpg')",
+        phone: "url('phone/charlie.jpg')",
+        songName: "Cheating On You",
+        artistName: "Charlie Puth",
+        song: "music/charlie puth/cheating.mp3"
+    }
+];
+
+// function when window gets load
+window.onload = () => {
+    // sweet alert
+    // Swal.fire(
+    //     'Welcome\nTo My Music Page',
+    //     'To show/hide the Player<br><b>Click on the Heading Above.</b> ',
+    //     'success'
+    //   );
+    // to track the current playying song
+    track();
+}
+//variables to capture all the id's
+let index = 0;  //to keep the index of playing song
+let isplay = false; //to check song is playying or not
+let myAudio = document.getElementById('myAudio');
+let playButton = document.getElementById('playButton');
+let forward = document.getElementById('forward');
+let backward = document.getElementById('backward');
+let artist = document.getElementById("artist");
+let song = document.getElementById("song");
+var x = window.matchMedia("(max-width: 700px)");
+let controls = document.getElementById("controllers");
+let sound = true;
+let volume = document.getElementById('volume');
+let visible = document.getElementById("visible");
+let isDisplay = true;
+let count = document.getElementById("count");
+let isAutoPlay = false;
+let loop = document.getElementById('loop');
+let nextPlay = document.getElementById('nextPlay');
+let previousPlay = document.getElementById('previousPlay');
+let counterPlay = 1;
+let bar = document.getElementById('bar');
+
+// Functions declarartion goes here
+track = () => { //function to display the current track number
+    count.innerHTML = `${index + 1} / ${lists.length} <br> ${network()}`;
+}
+function playPause() {  // funtion to play pause music
+    if (isplay == false) {
+        playButton.className = 'fa fa-pause-circle ';
+        header.style.animationIterationCount = 'infinite';
+        myAudio.play();
+        isplay = true;
+    } else {
+        header.style.animationIterationCount = '0';
+        playButton.className = 'fa fa-play-circle '
+        myAudio.pause();
+        isplay = false;
+    }
+    track();
+}
+function toggle() {     //Function to switch between songs
+    track();
+    image();
+    header.style.animationIterationCount = 'infinite';
+    song.innerHTML = lists[index].songName;
+    artist.innerHTML = lists[index].artistName;
+    playButton.className = 'fa fa-pause-circle ';
+    myAudio.src = lists[index].song;
+    myAudio.play();
+    isplay = true;
+}
+function image() {  // To switch Wallpaper according to screen size
+    if (x.matches) {
+        body.style.backgroundImage = lists[index].phone;
+    } else {
+        body.style.backgroundImage = lists[index].image;
+    }
+}
+function playNext() {   // To play next song
+    index++;
+    if (index == lists.length) {
+        index = 0;
+    }
+    if (counterPlay == 2) {
+        index = Math.floor(Math.random() * lists.length);
+    }
+    toggle();
+}
+function playPrevious() {   // To play previous song
+    index--;
+    if (index == -1) {
+        index = lists.length - 1;
+    }
+    if (counterPlay == 2) {
+        index = Math.floor(Math.random() * lists.length);
+    }
+    toggle();
+}
+//function for forward song by 5 seconds goes here
+function Forward() {
+    myAudio.currentTime = myAudio.currentTime + 10;
+}
+//funtion for backward song by 5 seconds goes here
+function Backward() {
+    myAudio.currentTime = myAudio.currentTime - 10;
+}
+setInterval(() => { // progress bar
+    bar.style.width = `${(myAudio.currentTime / myAudio.duration) * 100}%`;
+    track();
+    automatic();
+}, 100);
+function network() {    //function network state goes here
+    var net = myAudio.networkState;
+    if (net == 0) {
+        return 'Loading...';
+    } else
+        if (net == 1) {
+            return '<i class="far fa-music"></i>';
+        } else
+            if (net == 2) {
+                return 'Fetching...';
+            } else {
+                return 'No Audio Source Found';
+            }
+}
+function automatic() {  //function for shuffle mode
+    if (counterPlay == 1) {
+        loop.className = 'fa fa-repeat';
+        myAudio.loop = false;
+        if (myAudio.ended) {
+            index++;
+            if (index == lists.length) {
+                index = 0;
+            }
+            toggle();
+        }
+    } else
+        if (counterPlay == 2) {
+            loop.className = 'fa fa-shuffle';
+            if (myAudio.ended) {
+                index = Math.floor(Math.random() * lists.length);
+                toggle();
+            }
+        } else
+        if(counterPlay == 3) {
+            myAudio.loop = true;
+            loop.className = 'fa fa-repeat-1';
+        }
+}
+
+// function calling goes here
+visible.onclick = () => {   // controls visibilty code goes here
+    controls.style.display = "flex";
+    visible.style.display = "none";
+    isDisplay = true;
+}
+document.getElementById('hide').onclick = () => {
+    if (isDisplay == true) {
+        controls.style.display = "none";
+        visible.style.display = "block";
+        isDisplay = false;
+    }
+}
+volume.onclick = () => {    // mute sound code goes here
+    if (sound) {
+        myAudio.muted = true;
+        volume.className = "fa fa-volume-xmark";
+        volume.style.color = 'red';
+        sound = false;
+    } else {
+        myAudio.muted = false;
+        volume.className = "fa fa-volume-high";
+        volume.style.color = 'white';
+        sound = true;
+    }
+}
+playButton.addEventListener('click', playPause);//Play/Pause button
+nextPlay.addEventListener('click', playNext);//next play click button
+previousPlay.addEventListener('click', playPrevious);//prvious play click button
+loop.onclick=()=>{  //Autoplay features code goes here
+    counterPlay++;
+    if(counterPlay == 4){
+        counterPlay = 1;
+    }
+}
+forward.addEventListener('click', Forward);//forward click button
+backward.addEventListener('click', Backward);// backward click button
