47b748fd06dfafa1b26d748a725eee9025cf321e
ShivamSharma3589 <85331338+ShivamSharma3589@users.noreply.github.com>
2021-07-12 23:03:57 +0530

    favicon related updates

--- mian.js
+++ mian.js
@@ -2,6 +2,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/arijit.jpg')",
         phone: "url('phone/arijit.jpg')",
+        favicon:'images/arijit.jpg',
         songName: "Ve Maahi",
         artistName: "Arijit Singh",
         song: "music/arijit singh/ve maahi.mp3"
@@ -9,6 +10,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/jubin.jpg')",
         phone: "url('phone/jubin.jpg')",
+        favicon:'images/jubin.jpg',
         songName: "Aankh Uthi Mohabbat Ne",
         artistName: "Jubin Nautiyal",
         song: "music/arijit singh/mohabat.mp3"
@@ -16,6 +18,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/maroon.jpg')",
         phone: "url('phone/maroon.jpg')",
+        favicon:'images/maroon.jpg',
         songName: "Stereo Hearts",
         artistName: "Maroon 5",
         song: "music/maroon 5/stereo hearts.mp3"
@@ -23,6 +26,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/maroon.jpg')",
         phone: "url('phone/maroon.jpg')",
+        favicon:'images/maroon.jpg',
         songName: "Sugar",
         artistName: "Maroon 5",
         song: "music/maroon 5/sugar.mp3"
@@ -30,6 +34,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/maroon.jpg')",
         phone: "url('phone/maroon.jpg')",
+        favicon:'images/maroon.jpg',
         songName: "Beautiful Mistakes",
         artistName: "Maroon 5",
         song: "music/maroon 5/beautiful mistakes.mp3"
@@ -37,6 +42,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/maroon.jpg')",
         phone: "url('phone/maroon.jpg')",
+        favicon:'images/maroon.jpg',
         songName: "Animals",
         artistName: "Maroon 5",
         song: "music/maroon 5/animals.mp3"
@@ -44,6 +50,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/maroon.jpg')",
         phone: "url('phone/maroon.jpg')",
+        favicon:'images/maroon.jpg',
         songName: "Payphone",
         artistName: "Maroon 5",
         song: "music/maroon 5/payphone.mp3"
@@ -51,6 +58,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Levitating",
         artistName: "Dua Lipa",
         song: "music/dua lipa/levitating.m4a"
@@ -58,6 +66,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Don't Start Now",
         artistName: "Dua Lipa",
         song: "music/dua lipa/dont start now.m4a"
@@ -65,6 +74,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Break My Heart",
         artistName: "Dua Lipa",
         song: "music/dua lipa/break my heart.m4a"
@@ -72,6 +82,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "IDGAF",
         artistName: "Dua Lipa",
         song: "music/dua lipa/idgaf.m4a"
@@ -79,6 +90,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Cool",
         artistName: "Dua Lipa",
         song: "music/dua lipa/cool.m4a"
@@ -86,6 +98,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "One Kiss",
         artistName: "Dua Lipa",
         song: "music/dua lipa/one kiss.m4a"
@@ -93,6 +106,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "No Lie",
         artistName: "Dua Lipa",
         song: "music/dua lipa/no lie.m4a"
@@ -100,6 +114,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Pretty Please",
         artistName: "Dua Lipa",
         song: "music/dua lipa/pretty please.m4a"
@@ -107,6 +122,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Love Again",
         artistName: "Dua Lipa",
         song: "music/dua lipa/love again.m4a"
@@ -114,6 +130,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/dua.jpg')",
         phone: "url('phone/dua.jpg')",
+        favicon:'images/dua.jpg',
         songName: "Hallucinate",
         artistName: "Dua Lipa",
         song: "music/dua lipa/hallucinate.m4a"
@@ -121,6 +138,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/taylor.jpg')",
         phone: "url('phone/taylor.jpg')",
+        favicon:'images/taylor.jpg',
         songName: "Blank Space",
         artistName: "Taylor Swift",
         song: "music/taylor swift/blank space.mp3"
@@ -128,6 +146,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/taylor.jpg')",
         phone: "url('phone/taylor.jpg')",
+        favicon:'images/taylor.jpg',
         songName: "What You Made Me Do",
         artistName: "Taylor Swift",
         song: "music/taylor swift/look what you made me do.mp3"
@@ -135,6 +154,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/taylor.jpg')",
         phone: "url('phone/taylor.jpg')",
+        favicon:'images/taylor.jpg',
         songName: "Love Story",
         artistName: "Taylor Swift",
         song: "music/taylor swift/love story.mp3"
@@ -142,6 +162,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/taylor.jpg')",
         phone: "url('phone/taylor.jpg')",
+        favicon:'images/taylor.jpg',
         songName: "Wildest Dream",
         artistName: "Taylor Swift",
         song: "music/taylor swift/wildest dream.mp3"
@@ -149,6 +170,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/taylor.jpg')",
         phone: "url('phone/taylor.jpg')",
+        favicon:'images/taylor.jpg',
         songName: "Willow",
         artistName: "Taylor Swift",
         song: "music/taylor swift/willow.mp3"
@@ -156,6 +178,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "Call You Mine",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/call u mine.mp3"
@@ -163,6 +186,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "Break My Heart",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/break my heart.mp3"
@@ -170,6 +194,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "Sacrifice",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/sacrifice.mp3"
@@ -177,6 +202,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "Hey Mama",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/hey mama.mp3"
@@ -184,6 +210,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "I Got You",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/i got you.mp3"
@@ -191,6 +218,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "I'm a Mess",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/i m a mess.mp3"
@@ -198,6 +226,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/bebe.jpg')",
         phone: "url('phone/bebe.jpg')",
+        favicon:'images/bebe.jpg',
         songName: "No Broken Hearts",
         artistName: "Bebe Rexha",
         song: "music/bebe rexha/no broken hearts.m4a"
@@ -205,6 +234,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/ariana.jpg')",
         phone: "url('phone/ariana.jpg')",
+        favicon:'images/ariana.jpg',
         songName: "Love Me Harder",
         artistName: "Ariana Grande",
         song: "music/ariana grande/love me harder.mp3"
@@ -212,6 +242,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/ariana.jpg')",
         phone: "url('phone/ariana.jpg')",
+        favicon:'images/ariana.jpg',
         songName: "7 Rings",
         artistName: "Ariana Grande",
         song: "music/ariana grande/7 rings.mp3"
@@ -219,6 +250,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/ariana.jpg')",
         phone: "url('phone/ariana.jpg')",
+        favicon:'images/ariana.jpg',
         songName: "Into You",
         artistName: "Ariana Grande",
         song: "music/ariana grande/into you.mp3"
@@ -226,6 +258,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/ariana.jpg')",
         phone: "url('phone/ariana.jpg')",
+        favicon:'images/ariana.jpg',
         songName: "Positions",
         artistName: "Ariana Grande",
         song: "music/ariana grande/positions.mp3"
@@ -233,6 +266,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/ariana.jpg')",
         phone: "url('phone/ariana.jpg')",
+        favicon:'images/ariana.jpg',
         songName: "Rain On Me",
         artistName: "Ariana Grande",
         song: "music/ariana grande/rain on me.m4a"
@@ -240,6 +274,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Peaches",
         artistName: "Justin Bieber",
         song: "music/justin bieber/peaches.mp3"
@@ -247,6 +282,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Company",
         artistName: "Justin Bieber",
         song: "music/justin bieber/company.mp3"
@@ -254,6 +290,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Despacito",
         artistName: "Justin Bieber",
         song: "music/justin bieber/despacito.mp3"
@@ -261,6 +298,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Intentions",
         artistName: "Justin Bieber",
         song: "music/justin bieber/intentions.m4a"
@@ -268,6 +306,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Let Me Love You",
         artistName: "Justin Bieber",
         song: "music/justin bieber/let me love you.mp3"
@@ -275,6 +314,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/justin.jpg')",
         phone: "url('phone/justin.jpg')",
+        favicon:'images/justin.jpg',
         songName: "Sorry",
         artistName: "Justin Bieber",
         song: "music/justin bieber/Sorry.mp3"
@@ -282,6 +322,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "On My Way",
         artistName: "Alan Walker",
         song: "music/alan walker/on my way.mp3"
@@ -289,6 +330,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Alone",
         artistName: "Alan Walker",
         song: "music/alan walker/alone.mp3"
@@ -296,6 +338,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Alone Part II",
         artistName: "Alan Walker",
         song: "music/alan walker/alone2.mp3"
@@ -303,6 +346,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Choir",
         artistName: "Alan Walker",
         song: "music/alan walker/choir.mp3"
@@ -310,6 +354,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Darkside",
         artistName: "Alan Walker",
         song: "music/alan walker/darkside.mp3"
@@ -317,6 +362,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Faded",
         artistName: "Alan Walker",
         song: "music/alan walker/faded.mp3"
@@ -324,6 +370,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/alan.jpg')",
         phone: "url('phone/alan.jpg')",
+        favicon:'images/alan.jpg',
         songName: "Ignite",
         artistName: "Alan Walker",
         song: "music/alan walker/ignite.mp3"
@@ -331,6 +378,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/chain.jpg')",
         phone: "url('phone/chain.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Closer",
         artistName: "Chain Smokers",
         song: "music/chain smoker/closer.mp3"
@@ -338,6 +386,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/chain.jpg')",
         phone: "url('phone/chain.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Don't Let Me Down",
         artistName: "Chain Smokers",
         song: "music/chain smoker/dont let me down.mp3"
@@ -345,6 +394,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/chain.jpg')",
         phone: "url('phone/chain.jpg')",
+        favicon:'images/chain.jpg',
         songName: "All We Know",
         artistName: "Chain Smokers",
         song: "music/chain smoker/all we know.mp3"
@@ -352,6 +402,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/chain.jpg')",
         phone: "url('phone/chain.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Hope",
         artistName: "Chain Smokers",
         song: "music/chain smoker/hope.mp3"
@@ -359,6 +410,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/chain.jpg')",
         phone: "url('phone/chain.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Something Just Like This",
         artistName: "Chain Smokers",
         song: "music/chain smoker/closer.mp3"
@@ -366,6 +418,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/charlie.jpg')",
         phone: "url('phone/charlie.jpg')",
+        favicon:'images/chain.jpg',
         songName: "We Don't Talk Anymore",
         artistName: "Charlie Puth",
         song: "music/charlie puth/we dont talk.mp3"
@@ -373,6 +426,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/charlie.jpg')",
         phone: "url('phone/charlie.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Attention",
         artistName: "Charlie Puth",
         song: "music/charlie puth/attention.mp3"
@@ -380,6 +434,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/charlie.jpg')",
         phone: "url('phone/charlie.jpg')",
+        favicon:'images/chain.jpg',
         songName: "One Call Away",
         artistName: "Charlie Puth",
         song: "music/charlie puth/one call away.mp3"
@@ -387,6 +442,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/charlie.jpg')",
         phone: "url('phone/charlie.jpg')",
+        favicon:'images/chain.jpg',
         songName: "How Long",
         artistName: "Charlie Puth",
         song: "music/charlie puth/how long.mp3"
@@ -394,6 +450,7 @@ const lists = [     // list goes up here
     {
         image: "url('images/charlie.jpg')",
         phone: "url('phone/charlie.jpg')",
+        favicon:'images/chain.jpg',
         songName: "Cheating On You",
         artistName: "Charlie Puth",
         song: "music/charlie puth/cheating.mp3"
@@ -404,7 +461,7 @@ const lists = [     // list goes up here
 window.onload = () => {
     Swal.fire({       // sweet alert
         title: "Welcome\nTo My Music Page",
-        text: "The Player is Ready To Use.",
+        text: "<b>The Player is Ready To Use</b>",
         icon: "success",
     });
     track();     // to track the current playying song
@@ -431,6 +488,7 @@ let nextPlay = document.getElementById('nextPlay');
 let previousPlay = document.getElementById('previousPlay');
 let counterPlay = 1;
 let bar = document.getElementById('bar');
+let favicon = document.getElementById('favicon');
 
 // Functions declarartion goes here
 track = () => { //function to display the current track number
@@ -464,6 +522,7 @@ function toggle() {     //Function to switch between songs
 function image() {  // To switch Wallpaper according to screen size
     if (x.matches) {
         body.style.backgroundImage = lists[index].phone;
+        favicon.setAttribute('href',lists[index].favicon);
     } else {
         body.style.backgroundImage = lists[index].image;
     }
